8e884e3a3df598dce7fb2795275433f45a0f3ae8
Francisco Ramos <fro.profesional@gmail.com>
2023-04-04 15:51:42 -0700

    Added install and uninstall tasks

--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,17 @@
 import { Denomander, logger } from "../deps.ts";
 import { addCommands } from "./utils/cli.ts";
+import * as denojson from "../deno.json" assert { type: "json" };
 import {
   command as init,
   options as initOptions,
 } from "./commands/init/init.ts";
 
+const { name, version, description } = denojson.default;
+
 let program = new Denomander({
-  app_name: "Clau CLI",
-  app_version: "0.0.0",
+  app_name: name,
+  app_version: version,
+  app_description: description,
 });
 
 const commands = [
